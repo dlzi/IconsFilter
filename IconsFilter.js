d434485d02436a9d95ffd656699b36a866faae2b
Daniel Zilli <1160818+dlzi@users.noreply.github.com>
2024-06-20 10:36:44 -0300

    Initial commit

--- IconsFilter.js
+++ IconsFilter.js
@@ -1,15 +1,32 @@
-$(function() {
-	$('.InputfieldIconAll').before('<div id="icons-filter" class="hidden"><input id="icons-filter-input" type="text"></div>');
+$(document).ready(function() {
+    // Insert filter input before icons
+    $('.InputfieldIconAll').before('<div id="icons-filter" class="hidden"><input id="icons-filter-input" type="text" placeholder="Filter icons..."></div>');
 
-	$('a.InputfieldIconShowAll').click(function() {
-		$('#icons-filter').toggleClass('hidden');
-		$('#icons-filter-input').val('');
-	});
+    // Toggle the filter input visibility
+    $('a.InputfieldIconShowAll').click(function() {
+        $('#icons-filter').toggleClass('hidden');
+        $('#icons-filter-input').val('');
 
-	$('.InputfieldIcon').on('keyup', '#icons-filter-input', function(){
-		var filter_value = $(this).val();
-		var $icons = $('.InputfieldIconAll i');
-		$icons.parent('span').removeClass('hidden');
-		if(filter_value) $icons.not('[title*=' + filter_value + ']').parent('span').addClass('hidden');
-	});
+        // Show all icons when filter input is shown
+        $('.InputfieldIconAll i').parent('div').removeClass('hidden');
+
+        // Focus on the input field
+        if (!$('#icons-filter').hasClass('hidden')) {
+            $('#icons-filter-input').focus();
+        }
+    });
+
+    // Filter icons based on input value
+    $('.InputfieldIcon').on('keyup', '#icons-filter-input', function() {
+        var filterValue = $(this).val().toLowerCase();
+        var $icons = $('.InputfieldIconAll i');
+        $icons.each(function() {
+            var title = $(this).attr('title').toLowerCase();
+            if (title.indexOf(filterValue) === -1) {
+                $(this).addClass('hidden');
+            } else {
+                $(this).removeClass('hidden');
+            }
+        });
+    });
 });
